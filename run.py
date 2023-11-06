01c1bf168c6a88dc40bda4c102d4a3588417fca7
ceecedy <ceeceddy.pro@gmail.com>
2023-11-06 15:30:47 +0800

    previously known as blogpose file

---
+++ run.py
@@ -0,0 +1,6 @@
+# This is imported from the package blogpose. 
+# the app is in the __init__ of the blogpose module. 
+from blogpose import app 
+
+if __name__ == "__main__":
+    app.run(debug=True)
