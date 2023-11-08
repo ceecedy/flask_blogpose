0eaeabb8f7e5bc95f1927673afc98d280a06a065
ceecedy <ceeceddy.pro@gmail.com>
2023-11-08 09:57:07 +0800

    importing bcrypt and login manager. Instantiating both and initializing login view and message category

--- blogpose/__init__.py
+++ blogpose/__init__.py
@@ -3,6 +3,12 @@ from flask import Flask
 # check the description of the sql alchemy for informations. 
 from flask_sqlalchemy import SQLAlchemy
 
+# check the description of the Bcrypt for informations
+from flask_bcrypt import Bcrypt
+
+# check the description of LoginManager for informations 
+from flask_login import LoginManager
+
 app = Flask(__name__)
 # When using this two class that is made to be a form, we need to make a secret key. 
 # This will protect your users from modifying cookies and malicious attacks across web 
@@ -15,6 +21,18 @@ app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///site.db"
 
 # It is important to create a database instance. That's the use of SQL Alchemy. 
 db = SQLAlchemy(app)
+
+# creating an instance of bcrypt. 
+# using bcrypt - a type of hasing algorithm, will help the passwords of the users to get hashed. 
+bcrypt = Bcrypt(app)
+
+# creating an instance of LoginManager 
+# using LoginManager will ease up the development of login system of this project. 
+login_manager = LoginManager(app)
+# declare initially the login view of the app. 
+login_manager.login_view = "login"
+# declare initially this line below, if the user had access the account while not logged in. 
+login_manager.login_message_category = "info"
 
 # importing routes at this point to load up the routes at the init. 
 from blogpose import routes
