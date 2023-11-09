38efac65e9dba5f885e90e02105a799ff25bef47
ceecedy <ceeceddy.pro@gmail.com>
2023-11-09 18:27:06 +0800

    add db import for creation of database for initial run

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
 # This is imported from the package blogpose. 
 # the app is in the __init__ of the blogpose module. 
-from blogpose import app 
+from blogpose import app, db 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # This application context will run the db.create_all() if the databse is not yet existing. 
+    # else, if the tables exists, then it will not modify the existing tables. 
+    with app.app_context():
+        db.create_all()
+        app.run(debug=True)
