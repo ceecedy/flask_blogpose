8ab560117ec4a66eb54d5832f56f8bc2a2de2d06
ceecedy <ceeceddy.pro@gmail.com>
2023-11-17 09:26:53 +0800

    current app powered, not the main app. also always triggering token cleanup

--- run.py
+++ run.py
@@ -1,10 +1,18 @@
 # This is imported from the package blogpose. 
 # the app is in the __init__ of the blogpose module. 
-from blogpose import app, db 
+from blogpose import create_app, db 
+
+# importing User for token cleaning purposes. 
+from blogpose.models import User
+
+# getting an instance of create_app 
+app = create_app()
 
 if __name__ == "__main__":
     # This application context will run the db.create_all() if the databse is not yet existing. 
     # else, if the tables exists, then it will not modify the existing tables. 
     with app.app_context():
+        # Call the cleanup_expired_tokens method before each request
+        User.cleanup_expired_tokens()
         db.create_all()
         app.run(debug=True)
