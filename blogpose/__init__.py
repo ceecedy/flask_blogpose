b9a9f0642d0a6712a73bfae303b8bdfca4f5577a
ceecedy <ceeceddy.pro@gmail.com>
2023-11-17 09:27:32 +0800

    very main init file

--- blogpose/__init__.py
+++ blogpose/__init__.py
@@ -15,50 +15,59 @@ from flask_mail import Mail
 # importing oauth provider also here because the init is not reaching here. 
 from flask_oauthlib.provider import OAuth2Provider
 
-
-app = Flask(__name__)
-# When using this two class that is made to be a form, we need to make a secret key. 
-# This will protect your users from modifying cookies and malicious attacks across web 
-app.config['SECRET_KEY'] = "8b1bb1a28dbba839be48a867ccd0fc45"   
-
-# URI is where your database is located. 
-# we will use SQL lite for the development of this project and postgresql for the production. 
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///site.db"
-# triple slash is a relative path, that means the site.db will be created into the current file which is the project file. 
+# importing config class from config file.  
+from blogpose.config import Config
 
 # It is important to create a database instance. That's the use of SQL Alchemy. 
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 
 # creating an instance of bcrypt. 
 # using bcrypt - a type of hasing algorithm, will help the passwords of the users to get hashed. 
-bcrypt = Bcrypt(app)
+bcrypt = Bcrypt()
 
 # creating an instance of LoginManager 
 # using LoginManager will ease up the development of login system of this project. 
-login_manager = LoginManager(app)
+login_manager = LoginManager()
 # declare initially the login view of the app. 
 # this will be get by the routes.py if needed to login first. 
-login_manager.login_view = "login"
+login_manager.login_view = "users.login"
 # along with login_view, this is a flash message above the login_view. 
 # This will be get also by the routes.py if needed to login first. 
 # declare initially this line below, if the user had access the account form while not logged in. 
 login_manager.login_message_category = "info"
 
 # initializing the oauth. 
-oauth = OAuth2Provider(app)
-
-# email server configurations. 
-app.config["MAIL_SERVER"] = "smtp.googlemail.com"
-app.config["MAIL_PORT"] = "587"
-app.config["MAIL_USE_TLS"] = "True"
-
-# using environment variables for private access 
-app.config["MAIL_USERNAME"] = "jceee.castro@gmail.com"
-app.config["MAIL_PASSWORD"] = "rdbl qzno udxr jtlh"
+oauth = OAuth2Provider()
 
 # after setting up the mail server, we will now initialize it. 
-mail = Mail(app)
+mail = Mail()
 
-# importing routes at this point to load up the routes at the init. 
-from blogpose import routes
+# function to create the app. 
+def create_app(config_class = Config):
+    # creation of the application
+    app = Flask(__name__)
 
+    # this is to include the environment variables needed in this project. 
+    app.config.from_object(Config)
+    
+    # init_app is a method to use to initialize the extension.
+    # on this structure, this initializations are part of the application when this create_app func runs.
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+    
+    # importing routes at this point to load up the routes at the init. 
+    from blogpose.users.routes import users
+    from blogpose.posts.routes import posts
+    from blogpose.main.routes import main
+    
+    # registering blueprints of the packages 
+    # this is to let this "__init__.py" file to get the blueprints of sub-packages to let the sub-packages 
+    #   to be part of this "__init__.py" to become one big system. 
+    app.register_blueprint(main)
+    app.register_blueprint(users)
+    app.register_blueprint(posts)
+    
+    # finally return app when everything is within the app. 
+    return app
