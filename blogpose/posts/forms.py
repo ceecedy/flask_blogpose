9d9bf7db10ab851c24c066c17a13e8ed12cdffc0
ceecedy <ceeceddy.pro@gmail.com>
2023-12-01 17:11:20 +0800

    added image in the post forms

--- blogpose/posts/forms.py
+++ blogpose/posts/forms.py
@@ -5,6 +5,11 @@ from flask_wtf import FlaskForm
 # SubmitField class from wtforms is used to create a button specialized in submitting data.
 from wtforms import StringField, TextAreaField, SubmitField
 
+# this import is to take files as input of the clients. 
+# filefield is the class to accept the images. 
+# fileallowed is the class to check and limit the validations of the input images. 
+from flask_wtf.file import FileField, FileAllowed
+
 # importing a class that will make sure that the input should not be empty. 
 # the name of the class is "DataRequired" from module wtforms.validators. 
 from wtforms.validators import DataRequired, Length
@@ -24,6 +29,11 @@ class NewPost(FlaskForm):
     content = TextAreaField('Content', validators=[
         DataRequired(message = 'Content is required')
     ])
+    
+    # accept images 
+    # will pass it to the variable images 
+    image = FileField('Upload Image(s)', validators=[
+        FileAllowed(['jpg', 'png', 'jpeg', 'gif'], 'Images only!')])
     
     # accept submit button 
     # will pass it to the submit variable 
